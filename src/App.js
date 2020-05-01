a4b8371373e50e8ca2295ddd8d2919bae70e445e
Sebastian Castillo <sebastian.castillo.s@gmail.com>
2020-05-01 02:57:41 -0400

    Creando los primeros styled componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`;
+
 function App() {
-  return <Header titulo="Cotizador de Seguros" />;
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />;
+      <ContenedorFormulario></ContenedorFormulario>
+    </Contenedor>
+  );
 }
 
 export default App;
